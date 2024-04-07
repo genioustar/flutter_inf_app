913bff1e6f714bbf64e83fa6e0f023a0abed6d3d
master <genioustar>
2024-04-07 16:55:59 +0900

    섹션 1-2 텍스트필드 디자인하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inf_app/common/component/custom_text_form_field.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +14,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: const Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [CustomTextFormField()],
+        ),
+      ),
     );
   }
 }
