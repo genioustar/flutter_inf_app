036f72ef7adb9d315f89eacd1ac0ebbdd703423c
NY <genioustar@gmail.com>
2024-04-16 23:32:11 +0900

    appBar, BottomNavBar를 옵션으로 받아서 처리하게 추가

--- lib/common/component/layout/default_layout.dart
+++ lib/common/component/layout/default_layout.dart
@@ -3,17 +3,42 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Color? backgroundColor;
   final Widget widget;
+  // appBar 에 titlle을 넣을 수 있도록 추가
+  final String? title;
+  // bottomNavigationBar 선택적으로 넣을 수 있도록 추가
+  final Widget? bottomNavigationBar;
   const DefaultLayout({
     super.key,
     required this.widget,
     this.backgroundColor,
+    this.title,
+    this.bottomNavigationBar,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: widget,
+      bottomNavigationBar: bottomNavigationBar,
     );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          title!,
+          style:
+              const TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
+        ),
+      );
+    }
   }
 }
--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inf_app/common/component/layout/default_layout.dart';
+import 'package:flutter_inf_app/common/const/colors.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
 
   @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int index = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const DefaultLayout(
-      widget: Center(
+    return DefaultLayout(
+      title: '코팩 딜리버리',
+      widget: const Center(
         child: Text('Root Tab'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: BODY_TEXT_COLOR,
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
+        // 중요 기본 쉬프팅! - 선택하면 좀 더 크게 표시
+        type: BottomNavigationBarType.fixed,
+        onTap: (int index) {
+          setState(() {
+            this.index = index;
+          });
+        },
+        currentIndex: index,
+
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.fastfood_outlined), label: '음식'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.receipt_long_outlined), label: '주문'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline), label: '프로필'),
+        ],
       ),
     );
   }
